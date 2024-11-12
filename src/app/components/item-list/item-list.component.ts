01a176410b7f7d3d668b078dae702f4b7e8a2535
desarrollador01 <ivanandres26@hotmail.es>
2024-11-12 17:39:43 -0500

    Arreglo agrupamiento 7

--- src/app/components/item-list/item-list.component.ts
+++ src/app/components/item-list/item-list.component.ts
@@ -70,6 +70,7 @@ export class ItemListComponent {
       (data) => {
         this.items = data;
         this.groupItemsByMarca();
+        this.onFilterByMarca();
         this.marcas = [...new Set(this.items.map(item => item.Marca))];
         Swal.close();
         if (this.items.length === 0) {
